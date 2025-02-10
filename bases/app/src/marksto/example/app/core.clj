e5c772fc94d0ae496fd4f71acf4a86f9fe317643
Mark Sto <marksto@yourown.software>
2025-02-10 15:28:11 +0400

    Add some "logic" to the app so that one can test its operation

--- bases/app/src/marksto/example/app/core.clj
+++ bases/app/src/marksto/example/app/core.clj
@@ -2,7 +2,8 @@
   "Base API of an example app"
   (:require [clojure.tools.logging :as log]
             [marksto.example.app.config :as config]
-            [marksto.example.app.system.core :as system])
+            [marksto.example.app.system.core :as system]
+            [marksto.example.pg-ops.interface :as pg-ops])
   (:gen-class))
 
 ;; NB: Properly shutting production systems down is full of its own shenanigans,
@@ -16,12 +17,27 @@
 (defn- add-shutdown-hooks! []
   (Runtime/.addShutdownHook (Runtime/getRuntime) (Thread. ^Runnable shutdown!)))
 
+(defn launch! []
+  (add-shutdown-hooks!)
+  (system/init! (config/load!)))
+
+;;
+
+;; NB: To keep this example app minimalistic we just query the Postgres version.
+(defn do-something! []
+  (let [pg-version (-> (system/get-state)
+                       (get-in [:system :marksto.example.app.system/data-source])
+                       (pg-ops/query-version))]
+    (log/info (format "The DBMS version: %s" pg-version))))
+
+;;
+
 (defn -main [& _]
   ;; NB: Having the top-level `try-catch` block caters for both popular ways of
   ;;     launching the app — via 'clojure.main -m' and via vanilla '-jar' calls.
   (try
-    (add-shutdown-hooks!)
-    (system/init! (config/load!))
+    (launch!)
+    (do-something!)
     (catch Throwable t
       (log/error t "Failed to launch the app system")
       (System/exit 1))))
